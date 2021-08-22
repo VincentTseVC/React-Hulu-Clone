f0eeec12d9f5aff34ee7a12f32f0804a8ddb198f
VincentTse <from_vincent@hotmail.com>
2021-08-22 23:52:52 +0800

    added Thumbnails

--- components/Results.js
+++ components/Results.js
@@ -1,5 +1,13 @@
-const Results = () => {
-  return <div></div>;
+import Thumbnail from './Thumbnail';
+
+const Results = ({ movies }) => {
+  return (
+    <div>
+      {movies.map((movie) => (
+        <Thumbnail key={movie.id} result={movie} />
+      ))}
+    </div>
+  );
 };
 
 export default Results;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Results from '../components/Results';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home({ movies }) {
   return (
     <div>
       <Head>
@@ -14,11 +15,23 @@ export default function Home() {
 
       <Navbar />
 
-      <Results />
+      <Results movies={movies} />
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      movies: request.results,
+    },
+  };
 }
